630c3b790e58a8598517c1f60b5faf83f2b77824
aman101097 <aman.sharma@bankbuddy.ai>
2021-10-21 19:05:32 +0530

    learnt various modules importing e.g chalk, validator

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const add=require('./utils.js')
+// const add=require('./utils.js')
 
-const sum=add(4,-2)
+// const sum=add(4,-2)
 
-const fs=require('fs');
+// const fs=require('fs');
 
 
-fs.writeFileSync('notes.txt','Hi this is first line')
+// fs.writeFileSync('notes.txt','Hi this is first line')
 
-//appenfilesync is used to append to the file
+// //appenfilesync is used to append to the file
 
-console.log(sum)
+// console.log(sum)
+const validator=require('validator')
+const getNotes=require('./notes')
+
+const res=getNotes()
+const chalk = require('chalk');
+
+
+console.log(res)
+console.log(validator.isURL('dghhhhhhhgmailom'))
+console.log(chalk.green('Success'));
