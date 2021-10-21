adb6abd495c3b1ce1a9cfca8907a0df5ed59d37b
aman101097 <aman.sharma@bankbuddy.ai>
2021-10-21 18:43:32 +0530

    exporting function from one file to another

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
+const add=require('./utils.js')
+
+const sum=add(4,-2)
+
 const fs=require('fs');
+
+
 fs.writeFileSync('notes.txt','Hi this is first line')
 
 //appenfilesync is used to append to the file
+
+console.log(sum)
